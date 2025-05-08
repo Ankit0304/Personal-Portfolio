606d253e6e5d87f2f90380bd0967667508811441
Ankit Baingane <ankitbaingane157@gmail.com>
2025-05-09 03:08:53 +0530

    modification in template,settings and create model,view,admin files

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(About)
+admin.site.register(Education)
+admin.site.register(Skills)
+admin.site.register(Experience)
+admin.site.register(Projects)
+admin.site.register(Contact)
+admin.site.register(Link)
--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,6 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, This is starting section.")
+    about = About.objects.all()
+    education = Education.objects.all()
+    skills = Skills.objects.all()
+    experience = Experience.objects.all()
+    projects = Projects.objects.all()
+    contact = Contact.objects.all()
+    link = Link.objects.all()
+    
+    context = {
+        'about': about,
+        'education': education,
+        'skills': skills,
+        'experience': experience,
+        'projects': projects,
+        'contact': contact,
+        'link': link,
+    }
+    return render(request, 'index.html')
